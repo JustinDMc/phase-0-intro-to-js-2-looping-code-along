64775807768324094de58103c11a42c0453dc7d5
Justin <jdanielmcarthur@msn.com>
2021-04-27 10:26:45 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const name = ["Lisa", "kaitlin", "Jan"];
+const event = "surprise";
+
+function writeCards(name, event) {
+    let newArray = [];
+    for(let i = 0; i < name.length; i++) {
+    newArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+     }
+    return newArray;
+}
+
+function countDown(integer) {
+    let i = 10;
+    while (i >= 0) {
+        console.log(i--);
+    }
+}
+
