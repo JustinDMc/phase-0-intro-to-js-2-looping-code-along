349e000284d7e56bf22587e03cef10dfc7e0fe98
Justin <jdanielmcarthur@msn.com>
2021-05-20 12:44:03 -0700

    commit tree 45ca09a93b90afd41f4ceb3e00e9b6190eb32d63

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-const name = ["Lisa", "kaitlin", "Jan"];
-const event = "surprise";
+const names = ["Lisa", "Kaitlin", "Jan"] 
+const event = "birthday";
 
-function writeCards(name, event) {
-    let newArray = [];
-    for(let i = 0; i < name.length; i++) {
-    newArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
-     }
-    return newArray;
+function writeCards(names, event) {
+    let newNames = []
+    for (let i = 0; i < names.length; i ++) {
+    newNames.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return newNames;
 }
 
-function countDown(integer) {
+function countDown() {
     let i = 10;
     while (i >= 0) {
         console.log(i--);
     }
 }
-
